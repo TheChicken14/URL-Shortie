f6118ffa6c91ca698cdcb37a5f949dca3543b7e7
Wisse <wissehes@gmail.com>
2020-08-31 20:55:53 +0200

    serve react build files, otherwise run in api mode only

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express")
 const app = express()
 const bodyParser = require("body-parser")
 const cookieParser = require('cookie-parser');
+const fs = require("fs")
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -18,4 +19,18 @@ routes.forEach(r => {
     app.use(r.path, r.router)
 })
 
-app.listen(config.web.port, () => console.log(`App started. Listening on port ${config.web.port}`))
+if (!config.dev || process.argv[2] === "-prod") {
+    process.env.NODE_ENV = "production"
+    if (fs.existsSync("./build")) {
+        app.use(express.static('./build'))
+    } else {
+        console.warn("[WARNING] You don't have the frontend built. API Only mode is now active.")
+    }
+}
+
+app.listen(config.web.port, () => {
+    console.log(`App started. Listening on port ${config.web.port}`)
+    if (!config.dev || process.argv[2] === "-prod") {
+        console.log("Running in production mode!")
+    }
+})
