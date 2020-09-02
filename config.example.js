34ce68badbddaa708cdfd10e11c760b26981099d
Wisse <wissehes@gmail.com>
2020-09-02 14:00:09 +0200

    added proxy option to example config

--- config.example.js
+++ config.example.js
@@ -6,5 +6,6 @@ module.exports = {
         mongouri: '' // MongoDB URI
     },
     secret: 'your-secret', // Secret for JWT Token
+    proxy: false, // Set to true if using URL-Shortie behind a proxy
     dev: false // Set to true if working with development env
 }
