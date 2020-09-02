af2bc2c1f32171a1d5de5361e89fd34007b99c89
Wisse <wissehes@gmail.com>
2020-09-02 17:21:47 +0200

    Send UI file when reserved url is visited

--- routes/redirect.js
+++ routes/redirect.js
@@ -3,7 +3,14 @@ const router = express.Router()
 
 const Url = require('../db/models/Url')
 
+const reservedUrls = require("../reservedUrls")
+
+var path = require('path');
+
 router.get('/:id', async (req, res) => {
+    if (reservedUrls.indexOf(req.params.id.toLowerCase()) !== -1 && process.env.NODE_ENV === 'production') {
+        return res.sendFile(path.resolve("./build/index.html"))
+    }
     const shortUrl = await Url.findOne({ shortCode: req.params.id })
     if (shortUrl) {
         res.redirect(shortUrl.longUrl)
