3f0726d293ac6e75e37ee02e8f1c5f7b31d2079b
Wisse <wissehes@gmail.com>
2020-09-02 17:13:27 +0200

    Move all reserved URLs to seperate file

---
+++ reservedUrls.js
@@ -0,0 +1,13 @@
+/*
+ * This contains an array of urls that can't be used
+ * in short URLs and will be redirected to the UI
+ */
+
+module.exports = [
+    'admin',
+    'account',
+    'dashboard',
+    'login',
+    'logout',
+    'register'
+]
