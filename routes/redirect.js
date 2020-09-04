0759821837001a50ccee3922c09f91cf9883c31c
Wisse <wissehes@gmail.com>
2020-09-04 16:21:56 +0200

    Create visit document when visiting link

--- routes/redirect.js
+++ routes/redirect.js
@@ -2,20 +2,31 @@ const express = require('express')
 const router = express.Router()
 
 const Url = require('../db/models/Url')
-
+const Visit = require('../db/models/Visit')
 const reservedUrls = require("../reservedUrls")
+const useragent = require('express-useragent');
 
 var path = require('path');
 
-router.get('/:id', async (req, res) => {
+const isBot = require("isbot")
+
+router.get('/:id', useragent.express(), async (req, res) => {
     if (reservedUrls.indexOf(req.params.id.toLowerCase()) !== -1 && process.env.NODE_ENV === 'production') {
         return res.sendFile(path.resolve("./build/index.html"))
     }
     const shortUrl = await Url.findOne({ shortCode: req.params.id })
     if (shortUrl) {
         res.redirect(shortUrl.longUrl)
-        shortUrl.clickCount++
-        await shortUrl.save()
+        const isbot = isBot(req.get['user-agent'])
+        console.log(req.useragent)
+        new Visit({
+            shortCode: shortUrl.shortCode,
+            ip: req.ip,
+            referrer: req.get("Referrer") || "Unknown",
+            browser: !isbot && req.useragent.browser ? req.useragent.browser.toLowerCase() : "",
+            os: !isbot && req.useragent.os ? req.useragent.os.toLowerCase() : "",
+            isBot: isbot
+        }).save()
     } else {
         res.status(404).json({
             message: "Not found!"
