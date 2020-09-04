c5b89d57f37185f9a7f88bdc05fafb279ad533fb
Wisse <wissehes@gmail.com>
2020-09-04 19:19:55 +0200

    add my stuff to the router

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,23 +1,44 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import Dashboard from '../views/Dashboard.vue'
+import store from '../store'
 
 Vue.use(VueRouter)
 
-  const routes = [
+const withAuth = async (to, from, next) => {
+  await store.dispatch("checkToken")
+  if (!store.state.loggedIn) {
+    next({ name: 'Login' })
+  } else next()
+}
+
+const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
   },
   {
-    path: '/about',
+    path: '/login',
+    name: "Login",
+    component: Login
+  },
+  {
+    path: '/dashboard',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    component: Dashboard,
+    beforeEnter: withAuth
+  },
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
 ]
 
 const router = new VueRouter({
